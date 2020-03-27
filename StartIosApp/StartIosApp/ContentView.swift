0bbeecdb408a743248aa302030795aefedc45a62
VintageAppMaker <31234716+VintageAppMaker@users.noreply.github.com>
2020-03-27 14:04:01 +0900

    .

---
+++ StartIosApp/StartIosApp/ContentView.swift
@@ -0,0 +1,122 @@
+//
+//  ContentView.swift
+//  StartIosApp
+//
+//  Created by adsloader on 2020/03/24.
+//  Copyright © 2020 adsloader. All rights reserved.
+//
+
+import SwiftUI
+
+/*
+   #1 목적: 선학습없이 검색하며 찾아보는 SwiftUI
+   
+   1. XCode 11부터는 SwiftUI가 기본설정
+   2. 처음접하는 개발자에게는 Web 개발환경 같음
+   3. 러닝커브가 완곡하다고 함(OO보다 쉽다는 의미)
+ 
+ */
+
+// 0. ContentView는 화면에 그려질
+struct ContentView: View {
+    
+    // 1.
+    // swift 5.1부터 적용된 어노테이션 @State
+    // View에서,
+    // 값변경시 바로 화면에 적용해야 할 때 필수임.
+    // View 밖에서는 사용못함(private과 유사)
+    @State var title: String = "Swift Example"
+    @State var count : Int = 0
+    @GestureState var isLongPressed = false
+    
+    // 알럿처리
+    @State var isAlert = false
+    
+    // 2. some 키워드는 제너릭으로 이해하는 것이
+    // 초기개발에 도움이 됨
+    var body: some View {
+        
+        // 3. LongPressGesture
+        // @State로 선언된 변수를
+        // 바인딩(값변경 -> 화면전환)하고자 한다면 $변수 사용해야 함.
+        let longPress = LongPressGesture()
+            .updating($isLongPressed) { value, state, transcation in
+                state = value
+        }
+        
+
+        // 4. View를 넘김
+        // 직접 코딩이 가능하다. 그러나
+        // canvas의 미리보기에서 View를 선택 후,
+        // command + click 하면 좀 더 직관적으로
+        // 사용가능하다.
+        
+        // VStack은 Android의 버티컬 속성을 가진 LinearLayout과 같은
+        // 기능을 한다.
+        return VStack(alignment: .leading) {
+            // 텍스트
+            Text("\(title)")
+                .font(.title)
+                .bold()
+                .padding(.all, 4.0)
+            
+            // 버튼 1.
+            // Button 생성시 이름없는 {}는 Text를 리턴하는 초기화 함수이다. 실제이름은 label이다.
+            // action은 눌렀을 경우, 실행되는 콜백함수이다.
+            Button(action: {self.onIncrease()} ) {
+                makeButtonText(s : "Click me +1", c: Color.red)
+            }.padding(.all, 4.0)
+                
+            // 길게 누르기 이벤트를 처리하는 텍스트
+            // scaleEffect에서는 isLongPressed에 바인딩하여 값이 변경될 시, 확대축소
+            // gesture에는 이벤트핸들러를 지정
+            Text("I'm Text. plz longpress ... ")
+                .padding(.all, 4.0)
+                .font(.subheadline)
+                .scaleEffect(isLongPressed ? 2.1 : 1)
+                .gesture(longPress)
+            
+            // 버튼 2.
+            Button(action: {self.onShowpopup()} ) {
+                makeButtonText(s : "Click me, UIAlertController()", c: Color.blue)
+            }.padding(.all, 4.0)
+            
+            // 버튼 3.
+            Button(action: { self.isAlert = true} ) {
+                makeButtonText(s : "Click me, SwiftUI Alert", c: Color.gray)
+            }.alert(isPresented: $isAlert) {
+                Alert(title: Text("SwiftUI"), message: Text("변수를 바인딩해야함"), dismissButton: .default(Text("이해했습니다. 귀찮네요..")))
+            }.padding(.all, 4.0)
+        }
+        // 전체크기로 조정
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        .background(Color.green)
+    }
+    
+    // 버튼1 Handler
+    func onIncrease (){
+        count = count + 1
+        self.title = "Swift Example \(self.count)"
+    }
+    
+    // 버튼 2 Handler (swiftUI Alert 아님)
+    func onShowpopup (){
+        NotificationCenter.default.post(name: Notification.Name("showAlert"), object: "button의 alert 메소드를 사용하는 것이 정석임\nUIKit과 호환을 위해 만든코드.\nSceneDelegate.swift에 기능추가함")
+    }
+}
+
+// View를 아래와 같이 함수로 만들어 사용하는 것이
+// 가독성 및 관리차원에서 좋다
+func makeButtonText(s : String, c :Color) -> Text{
+    return Text(s)
+        .bold()
+        .foregroundColor(c)
+}
+
+
+// 미리보기
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StartIosApp/StartIosApp/SceneDelegate.swift
@@ -0,0 +1,79 @@
+//
+//  SceneDelegate.swift
+//  StartIosApp
+//
+//  Created by adsloader on 2020/03/24.
+//  Copyright © 2020 adsloader. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+        
+        // 노티피케이션 관련 옵저저 코드를 추가함
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showAlert), name: Notification.Name("showAlert"), object: nil)
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+    
+    // swiftUI에서 UIKit alert을 사용하기 위해 구글링한 코드 
+    @objc private func showAlert(notification: NSNotification){
+        let msg: String = notification.object as! String
+        let alert =  UIAlertController(title: "Title", message: msg, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "οκ", style: .cancel) { (action) in
+        }
+        alert.addAction(cancelAction)
+        DispatchQueue.main.async {
+            self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+        }
+    }
+
+
+}
+
