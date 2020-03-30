9fbd4c1a1e2967e73eff81a241059b5405b04585
park <adsloader99@gmail.com>
2020-03-30 21:52:03 +0900

    .

--- StartIosApp/StartIosApp/Example2View.swift
+++ StartIosApp/StartIosApp/Example2View.swift
@@ -12,7 +12,8 @@ import Combine
 struct Example2View: View {
 
     @State private var name    : String = ""
-    @State private var message : String = ""
+    var animals = ["Dog", "Cat", "Bird", "영식이"]
+    @State private var selectedAnimal = 0
     
     // 초기화
     init(){
@@ -22,19 +23,38 @@ struct Example2View: View {
     var body: some View {
         
         return VStack(alignment: .leading){
-            Text(message)
-            // name과 바인딩되어 입력시마다 화면에 보여준다. 
+            
+            // name과 바인딩되어 입력시마다 화면에 보여준다.
             TextField("이름을 입력하세요", text: $name)
-            Text("안녕, \(name)!")
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            
+            // onTapGesture 적용. 2번탭
+            Text("안녕, \(name)!").onTapGesture(count: 2) {
+                self.name = "2번 tap 헀습니다."
+            }
+            
+            // VStack으로 감싸지않으면 코딩스타일 문제로
+            // 호출에러가 발생하기도 한다.
+            VStack {
+                // Picker 지정
+               Picker(selection: $selectedAnimal, label: Text("동물")) {
+                  ForEach(0 ..< animals.count) {
+                     Text(self.animals[$0])
+                  }
+               }
+               Text("선택동물: \(animals[selectedAnimal])")
+            }
+            
             
             // 크기를 지정한 Spacer
-            Spacer().frame(height: 200)
+            Spacer().frame(height: 20)
             
             // 글자색, 배경색
             // 글자크기 및 폰트
-            Text("줄간격이\n있는 문자열\n입니다.")
-                .font(.custom("SF Mono", size: 23)) //폰트, 크기
-                .kerning(20) // 자간
+            Text("줄간격, 자간설정을 한\n문자열로 테스트 한 것 입니다.")
+                .font(.custom("SF Mono", size: 13)) //폰트, 크기
+                .kerning(15) // 자간
                 .multilineTextAlignment(.trailing) // 멀티라인시 정렬
                 .lineSpacing(23) // 줄간격
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
@@ -45,12 +65,9 @@ struct Example2View: View {
             
             Spacer()
             
-            // 이미지 및 배경
-            Text("크기지정")
+            // 이미지에 배경색 설정
             Image("cropped-logo")
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                .font(.largeTitle)
-                .foregroundColor(.white)
                 .background( Color.red)
             
             // 배경에 이미지 넣기
@@ -63,8 +80,6 @@ struct Example2View: View {
             
         }.onAppear(){
             
-            // @State로 선언된 변수를 외부 모듈과 바인딩
-            ChangeStateValue(name : self.$message).modifyed(s : "loading 완료")
             
         }.padding(.all, 16.0)
     }
